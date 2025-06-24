4c4d247633593d8b499d14715297ee87c22096ef
jemal leon <jemalxd8@gmail.com>
2025-06-24 16:45:00 -0500

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html", "./**/*.html"], // asegúrate de que incluya tus archivos
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
